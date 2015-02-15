bfd335079efa3081ed5ac529457d6774a9e90f45
Mikulas <rullaf@gmail.com>
2015-02-15 18:27:10 +0100

    add run.sh default args

--- tests/run.sh
+++ tests/run.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 dir=$(cd `dirname $0` && pwd)
-$dir/../vendor/bin/tester -p php --setup $dir/inc/setup.php $@
+
+if [[ "$#" -lt "1" ]]; then
+	FILES="$dir/cases"
+else
+	FILES=$@
+fi
+
+"$dir/../vendor/bin/tester" -p php --setup "$dir/inc/setup.php" $FILES
